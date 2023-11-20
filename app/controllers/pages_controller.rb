21bd0b62a3ba406f1f41d5d20c95d85c1978dd03
alexis stephan <alexis-stephan@hotmail.fr>
2023-11-20 10:18:59 +0100

    remove algo action home pagescontroller
    Signed-off-by: alexis stephan <alexis-stephan@hotmail.fr>

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, except: [:home]
   def home
-    @user = current_user
+    # @user = current_user
   end
 end
